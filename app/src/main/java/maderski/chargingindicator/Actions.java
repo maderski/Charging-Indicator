3f521c1c59a861f6728e8f2df784b9e72be839a3
Jason Maderski <maderski@gmail.com>
2016-08-02 19:36:54 -0400

    small code clean up

--- app/src/main/java/maderski/chargingindicator/Actions.java
+++ app/src/main/java/maderski/chargingindicator/Actions.java
@@ -4,9 +4,9 @@ package maderski.chargingindicator;
  * Created by Jason on 8/2/16.
  */
 public interface Actions {
-    public void vibrate();
-    public void makeSound();
-    public void showToast(String message);
-    public void showNotification();
-    public void removeNotification();
+    void vibrate();
+    void makeSound();
+    void showToast(String message);
+    void showNotification();
+    void removeNotification();
 }
