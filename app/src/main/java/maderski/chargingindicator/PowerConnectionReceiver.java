819066252a0fa4668d537bdbd4c9b06e53ee2e89
Jason Maderski <maderski@gmail.com>
2016-09-05 16:24:23 -0400

    Made connected and disconnected tasks Async

--- app/src/main/java/maderski/chargingindicator/PowerConnectionReceiver.java
+++ app/src/main/java/maderski/chargingindicator/PowerConnectionReceiver.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -36,18 +37,58 @@ public class PowerConnectionReceiver extends BroadcastReceiver{
         switch (action){
             //When POWER_CONNECTED is received create a toast message saying Power Connected
             case Intent.ACTION_POWER_CONNECTED:
-                performActions.vibrate();
-                performActions.makeSound();
-                performActions.showToast("Power Connected");
+                new AsyncConnectedActions(performActions).execute();
                 context.startService(new Intent(context, BatteryService.class));
                 break;
             //When POWER_DISCONNECTED is received create a toast message saying Power Disconnected
             case Intent.ACTION_POWER_DISCONNECTED:
-                performActions.removeNotification();
-                performActions.showToast("Power Disconnected");
+                new AsyncDisconnectedActions(performActions).execute();
                 context.stopService(new Intent(context, BatteryService.class));
                 break;
         }
 
+    }
+
+    private class AsyncConnectedActions extends AsyncTask<Void, Void, Void>{
+
+        private PerformActions performActions;
+
+        public AsyncConnectedActions(PerformActions performActions){
+            this.performActions = performActions;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            performActions.showToast("Power Connected");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            performActions.vibrate();
+            performActions.makeSound();
+            return null;
+        }
+    }
+
+    private class AsyncDisconnectedActions extends AsyncTask<Void, Void, Void>{
+
+        private PerformActions performActions;
+
+        public AsyncDisconnectedActions(PerformActions performActions){
+            this.performActions = performActions;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            performActions.showToast("Power Disconnected");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            performActions.removeNotification();
+            return null;
+        }
     }
 }
