409cbd1583c8a480e32a69d07e3c75be136757e5
Jason Maderski <maderski@gmail.com>
2018-07-04 16:07:48 -0400

    pulled BubblesManager and BubbleView out of service and created CIBubblesHelper to handle removing and adding of CIBubble

---
+++ app/src/main/java/maderski/chargingindicator/helpers/CIBubblesHelper.kt
@@ -0,0 +1,53 @@
+package maderski.chargingindicator.helpers
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import com.txusballesteros.bubbles.BubblesManager
+import com.txusballesteros.bubbles.OnInitializedCallback
+import maderski.chargingindicator.R
+import maderski.chargingindicator.interfaces.LastXYCoordListener
+import maderski.chargingindicator.services.BatteryService
+import maderski.chargingindicator.sharedprefs.CIPreferences
+import maderski.chargingindicator.ui.custom.CustomBubbleLayout
+
+class CIBubblesHelper(private val context: Context) : OnInitializedCallback, LastXYCoordListener {
+    // Create BubblesManager Instance
+    private val mBubblesManager: BubblesManager = BubblesManager.Builder(context)
+            .setInitializationCallback(this)
+            .build()
+
+    // Once BubblesManager is initialized add the Charging Bolt Bubble
+    override fun onInitialized() {
+        addChargingBoltBubble()
+    }
+
+    fun addBubble() {
+        mBubblesManager.initialize()
+    }
+
+
+    fun removeBubble() {
+        mBubblesManager.recycle()
+    }
+
+    private fun addChargingBoltBubble() {
+        val lastXCoord = CIPreferences.getChargingBubbleX(context).toInt()
+        val lastYCoord = CIPreferences.getChargingBubbleY(context).toInt()
+
+        val bubbleView: CustomBubbleLayout = LayoutInflater.from(context).
+                inflate(R.layout.floating_widget_layout, null) as CustomBubbleLayout
+        bubbleView.setShouldStickToWall(false)
+        bubbleView.setLastXYCoordListener(this)
+
+        Log.d(BatteryService.TAG, "LAST XY COORD X: $lastXCoord Y: $lastYCoord")
+        mBubblesManager.addBubble(bubbleView, lastXCoord, lastYCoord)
+    }
+
+    // Save the last XY Coordinate that a user lifted there finger after moving the Bubble
+    override fun onLastXYCoord(x: Float, y: Float) {
+        Log.d(BatteryService.TAG, "SET LAST XY COORD X: $x Y: $y")
+        CIPreferences.setChargingBubbleX(context, x)
+        CIPreferences.setChargingBubbleY(context, y)
+    }
+}
--- app/src/main/java/maderski/chargingindicator/services/BatteryService.kt
+++ app/src/main/java/maderski/chargingindicator/services/BatteryService.kt
@@ -5,50 +5,25 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
 import android.util.Log
-import android.view.LayoutInflater
-import com.txusballesteros.bubbles.BubblesManager
 import maderski.chargingindicator.R
-import maderski.chargingindicator.interfaces.LastXYCoordListener
+import maderski.chargingindicator.helpers.CIBubblesHelper
 import maderski.chargingindicator.receivers.BatteryReceiver
 import maderski.chargingindicator.sharedprefs.CIPreferences
-import maderski.chargingindicator.ui.custom.CustomBubbleLayout
 import maderski.chargingindicator.utils.ServiceUtils
 
 class BatteryService : Service() {
     private val mBatteryReceiver = BatteryReceiver()
 
     private var mIsShowingFAB = false
-    private var mBubblesManager: BubblesManager? = null
+    private var mCIBubblesHelper: CIBubblesHelper? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val filter = IntentFilter()
         registerReceiver(mBatteryReceiver, filter)
         mIsShowingFAB = CIPreferences.getShowChargingBubble(this)
         if(mIsShowingFAB) {
-            mBubblesManager = BubblesManager.Builder(this)
-                    .setInitializationCallback {
-                        val bubbleView: CustomBubbleLayout = LayoutInflater.from(this).inflate(R.layout.floating_widget_layout, null) as CustomBubbleLayout
-                        bubbleView.setShouldStickToWall(false)
-                        bubbleView.setLastXYCoordListener(object: LastXYCoordListener{
-                            override fun onLastXYCoord(x: Float, y: Float) {
-                                Log.d(TAG, "SET LAST XY COORD X: $x Y: $y")
-                                CIPreferences.setChargingBubbleX(this@BatteryService, x)
-                                CIPreferences.setChargingBubbleY(this@BatteryService, y)
-                            }
-
-                        })
-
-                        if(mBubblesManager != null) {
-                            val lastXCoord = CIPreferences.getChargingBubbleX(this).toInt()
-                            val lastYCoord = CIPreferences.getChargingBubbleY(this).toInt()
-                            Log.d(TAG, "LAST XY COORD X: $lastXCoord Y: $lastYCoord")
-                            mBubblesManager?.addBubble(bubbleView, lastXCoord, lastYCoord)
-                        }
-                    }
-                    .build()
-            if(mBubblesManager != null) {
-                mBubblesManager?.initialize()
-            }
+            mCIBubblesHelper = CIBubblesHelper(this)
+            mCIBubblesHelper?.addBubble()
         }
 
         return START_NOT_STICKY
@@ -71,9 +46,7 @@ class BatteryService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         if(mIsShowingFAB) {
-            if(mBubblesManager != null) {
-                mBubblesManager?.recycle()
-            }
+            mCIBubblesHelper?.removeBubble()
         }
 
         stopForeground(true)
