6ed559b70d126f22baf0b4820de7a8e9e486d477
Jason Maderski <maderski@gmail.com>
2018-07-04 14:07:47 -0400

    code clean up

--- app/src/main/java/maderski/chargingindicator/services/BatteryService.kt
+++ app/src/main/java/maderski/chargingindicator/services/BatteryService.kt
@@ -5,8 +5,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
 import android.util.Log
-import android.view.*
-import com.txusballesteros.bubbles.BubbleLayout
+import android.view.LayoutInflater
 import com.txusballesteros.bubbles.BubblesManager
 import maderski.chargingindicator.R
 import maderski.chargingindicator.interfaces.LastXYCoordListener
@@ -18,11 +17,8 @@ import maderski.chargingindicator.utils.ServiceUtils
 class BatteryService : Service() {
     private val mBatteryReceiver = BatteryReceiver()
 
-    private var mLayoutParams: WindowManager.LayoutParams? = null
     private var mIsShowingFAB = false
     private var mBubblesManager: BubblesManager? = null
-    private var mWindowManager: WindowManager? = null
-    private var mFloatingWidgetView: View? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val filter = IntentFilter()
@@ -55,64 +51,8 @@ class BatteryService : Service() {
             }
         }
 
-//        if(mIsShowingFAB) {
-//            mWindowManager = getSystemService(android.content.Context.WINDOW_SERVICE) as WindowManager
-//            mFloatingWidgetView = LayoutInflater.from(this).inflate(R.layout.floating_widget_layout, null)
-//
-//            val windowType = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-//                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-//            else
-//                WindowManager.LayoutParams.TYPE_PHONE
-//
-//            val windowFlags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-//
-//            mLayoutParams = WindowManager.LayoutParams(
-//                    WindowManager.LayoutParams.WRAP_CONTENT,
-//                    WindowManager.LayoutParams.WRAP_CONTENT,
-//                    windowType,
-//                    windowFlags,
-//                    PixelFormat.TRANSPARENT
-//            )
-//
-//            // Set ImageView initial position
-//            mLayoutParams?.gravity = Gravity.CENTER
-//
-//            mFloatingWidgetView?.let {
-//                if(mWindowManager != null) {
-//                    mWindowManager?.addView(it, mLayoutParams)
-//                }
-//            }
-//
-//            setFloatingWidgetListeners()
-//        }
-
         return START_NOT_STICKY
     }
-
-//    private fun setFloatingWidgetListeners() {
-//        mFloatingWidgetView?.let {
-//            val chargingBoltIV: MovableFloatingImageView = it.findViewById(R.id.iv_charging_bolt)
-//            chargingBoltIV.setActionMoveListener(object: ActionMoveListener {
-//                override fun onActionMove(newX: Float, newY: Float) {
-//                    it.animate()
-//                            .x(newX)
-//                            .y(newY)
-//                            .setDuration(0)
-//                            .start()
-//                }
-//
-//            })
-//        }
-//    }
-
-//    private fun setViewDimensions(view: View, heightWidthParams: Int) {
-//        view.layoutParams = view.layoutParams.apply {
-//            width = heightWidthParams
-//            height = heightWidthParams
-//        }
-//
-//        mWindowManager?.updateViewLayout(view, view.layoutParams)
-//    }
 
     override fun onCreate() {
         super.onCreate()
@@ -134,11 +74,6 @@ class BatteryService : Service() {
             if(mBubblesManager != null) {
                 mBubblesManager?.recycle()
             }
-//            mFloatingWidgetView?.let {
-//                if(mWindowManager != null) {
-//                    mWindowManager?.removeView(it)
-//                }
-//            }
         }
 
         stopForeground(true)
