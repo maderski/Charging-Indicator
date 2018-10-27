83e7d90e699a771a1cf90ebc7b70560bdd734a53
Jason Maderski <maderski@gmail.com>
2018-10-27 14:38:28 -0400

    convert AboutActivity and SplashActivity to Kotlin

---
+++ app/src/main/java/maderski/chargingindicator/ui/activities/AboutActivity.kt
@@ -0,0 +1,48 @@
+package maderski.chargingindicator.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
+import android.widget.TextView
+import maderski.chargingindicator.R
+import java.util.*
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        supportActionBar?.let {
+            Objects.requireNonNull(it).setDisplayHomeAsUpEnabled(true)
+        }
+        setVersionText()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        onBackPressed()
+        return super.onOptionsItemSelected(item)
+    }
+
+    //Smooth fade transition from about activity back to the main activity
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(0, R.animator.fadeout)
+    }
+
+    //Displays version number
+    private fun setVersionText() {
+        try {
+            val versionTV: TextView = findViewById(R.id.versionTxt)
+            val pkgInfo = packageManager.getPackageInfo(packageName, 0)
+            val versionInfo = "v" + pkgInfo.versionName
+            versionTV.text = versionInfo
+        } catch (e: Exception) {
+            Log.e(TAG, e.message)
+        }
+    }
+
+    companion object {
+        private val TAG = AboutActivity::class.java.name
+    }
+}
